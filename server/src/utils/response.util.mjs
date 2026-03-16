a4179d3ff9135f6ebd9db410297962e314c244d2
Nidhi Anna Sequeira <sequeiranidhianna@gmail.com>
2026-03-16 12:12:49 +0530

    feat: add response utility functions for success and error handling

---
+++ server/src/utils/response.util.mjs
@@ -0,0 +1,16 @@
+// server/src/util/response.util.mjs
+export const sendSuccessResponse = (res, statusCode, message, data = null) => {
+  res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+  });
+};
+
+export const sendErrorResponse = (res, statusCode, message, error = null) => {
+  res.status(statusCode).json({
+    success: false,
+    message,
+    error,
+  });
+};
